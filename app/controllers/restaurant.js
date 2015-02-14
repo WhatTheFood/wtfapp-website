42058d7bdfcfeed01328cedbe4dec1d8cfef181c
Martin Jeannot <martin.jeannot@outlook.com>
2015-02-14 05:45:43 +0100

    Add control flow for geospatial querying

--- app/controllers/restaurant.js
+++ app/controllers/restaurant.js
@@ -12,14 +12,19 @@ exports.getRestaurants = function (req, res){
       return console.log(err);
     }
   });
-}
+};
 
-exports.getRestaurant = function (req, res){
-  return RestaurantModel.findOne({"id": req.params.id}, function (err, restaurant) {
-    if (!err) {
-      return res.send(restaurant);
+exports.getRestaurant = function (req, res) {
+    if (req.query.lat && req.query.lng) {
+        // todo : geospatial querying
     } else {
-      return console.log(err);
+        return RestaurantModel.findOne({"id": req.params.id}, function (err, restaurant) {
+            console.log(req.query);
+            if (!err) {
+                return res.send(restaurant);
+            } else {
+                return console.log(err);
+            }
+        });
     }
-  });
-}
+};
