21ee6944eb14b3d5c8f2c2f806addc8155e7b19b
Martin Jeannot <martin.jeannot@outlook.com>
2015-02-14 14:45:57 +0100

    Fix mongoose auto-population of queue.timeSlots as an empty array

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -88,7 +88,7 @@ restaurantSchema.pre('save', function(next) {
     };
 
     // set queue info upon first insertion
-    if (!restaurant.queue) {
+    if (!restaurant.queue || restaurant.queue.length === 0) {
         restaurant.queue = {
             value: 0,
             votes: 0,
