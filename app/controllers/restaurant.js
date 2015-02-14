d680600eb2d4d7140323f1419e0f237cc6be7516
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-02-14 04:41:27 +0100

    Improving the consistency between User and Restaurant models usage.

--- app/controllers/restaurant.js
+++ app/controllers/restaurant.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-require('../models/restaurant')
+var RestaurantModel = require('../models/restaurant');
 
 /* get restaurants */
 exports.getRestaurants = function (req, res){
