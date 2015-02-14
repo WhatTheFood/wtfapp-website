b2f25d5003dcc2256b1b0bb36b5e10955c9257b4
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-02-14 03:35:17 +0100

    Adding authentication for restaurants route.

--- app/routes/restaurants.js
+++ app/routes/restaurants.js
@@ -4,10 +4,10 @@ var restaurantController = require('../controllers/restaurant');
 
 /* users */
 router.route('/')
-  .get(restaurantController.getRestaurants);
+  .get(authController.isAuthenticated, restaurantController.getRestaurants);
 
 /* user */
 router.route('/:id')
-  .get(restaurantController.getRestaurant);
+  .get(authController.isAuthenticated, restaurantController.getRestaurant);
 
 module.exports = router;
