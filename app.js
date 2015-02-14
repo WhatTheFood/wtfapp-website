db84931d79dedbe918c0c210e97eab4963ff4537
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-02-14 12:19:03 +0100

    Temporarily removing authentication.

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// allow cross domain
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+
 // Use the passport package in our application
 app.use(passport.initialize());
 
