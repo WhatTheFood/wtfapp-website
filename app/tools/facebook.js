ed339d5c035df5fdc140dc5b3f9d2a6b7001804a
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-03-30 01:13:05 +0200

    Removing Facebook test token.

--- app/tools/facebook.js
+++ app/tools/facebook.js
@@ -2,7 +2,6 @@ var FB = require('fb');
 
 exports.updateUserBasicInfos = function(user, callback) {
 
-    //token = "CAAIMhwO1ZCbsBAGNSczxzT6gPODvhvsNc7yw68hcDrIX2rSwSexl5sL63HfT8ZBtZBPxl42wquqZAUqwnGj0arKcbiP1kRlsCNdhCSuVDh6ErheEPV3vXSLanHXaZCkvWyJxAGMOhOtjTThn4fx1ZBZCTnsL2VlPUDYPPWhhGC4DRUFBo4XHcTLMZCuLp7LOm0AHKltZB6NYD5OhZB7ZC00h5IF1Nsj7rUqlO8ZD";
     token = user.facebook_token;
 
     FB.api('me',
@@ -47,7 +46,6 @@ exports.updateUserBasicInfos = function(user, callback) {
 
 exports.getUserFacebookFriends = function(user, callback) {
 
-    //token = "CAAIMhwO1ZCbsBACZCtZB40H4icCaobtWH24991dd7bT4z6CPvv4MRhNuMmvcux11WbQZC7CV5bC3OG4fcelk49ArJAZCONBZCNJ5OANZAB14wgAy0q7hGFBkdZBkJZCziZA6SfHBdWuMJJ8IuV5J3HVFtnSTMGzGEA1LGGz7ut00mtMva5D0qLmp0ZBUZBjyFsrD3uzG2YBgH1vNzcTx8EBf3ijkZCgfLwJMkZCnQZD";
     token = user.facebook_token;
 
     FB.api('/me/friends',
