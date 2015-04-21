b2f7a858e8f12dfed34414c150bdda4a2af46739
Adrien Giboire <adrien.giboire@gmail.com>
2015-04-21 12:55:45 +0200

    style: js coding conventions

--- app/controllers/restaurant.js
+++ app/controllers/restaurant.js
@@ -14,27 +14,27 @@ var RestaurantModel = require('../models/restaurant');
  * @returns {*}
  */
 exports.getRestaurantWFeedback = function (req, res) {
-    return exports.getRestaurant(req, res, true);
-}
+  return exports.getRestaurant(req, res, true);
+};
 exports.getRestaurantWOFeedback = function (req, res) {
-    return exports.getRestaurant(req, res, false);
-}
+  return exports.getRestaurant(req, res, false);
+};
 exports.getRestaurant = function (req, res, feedback) {
 
-    process_restaurant = function (err, restaurant) {
-            console.log(req.query);
-            if (!err) {
-                return res.send(restaurant);
-            } else {
-                console.log(err);
-                return res.status(400).send(err);
-            }
-        };
+  process_restaurant = function (err, restaurant) {
+    console.log(req.query);
+    if (!err) {
+      return res.send(restaurant);
+    } else {
+      console.log(err);
+      return res.status(400).send(err);
+    }
+  };
 
-    if (feedback)
-        return RestaurantModel.findOne({"id": req.params.id}).select("id").select("title").select("lat").select("lon").select("geolocation").select("distance").select("area").select("opening").select("closing").select("accessibility").select("wifi").select("shortdesc").select("description").select("access").select("operationalhours").select("contact").select("photo").select("payment").select("queue").select("menus").exec(process_restaurant);
-    else
-        return RestaurantModel.findOne({"id": req.params.id}, process_restaurant);
+  if (feedback)
+    return RestaurantModel.findOne({"id": req.params.id}).select("id").select("title").select("lat").select("lon").select("geolocation").select("distance").select("area").select("opening").select("closing").select("accessibility").select("wifi").select("shortdesc").select("description").select("access").select("operationalhours").select("contact").select("photo").select("payment").select("queue").select("menus").exec(process_restaurant);
+  else
+    return RestaurantModel.findOne({"id": req.params.id}, process_restaurant);
 };
 
 /**
@@ -47,38 +47,38 @@ exports.getRestaurant = function (req, res, feedback) {
 
 exports.getRestaurants = function (req, res) {
 
-    if (req.query.lat && req.query.lng) { // geospatial querying
-        var geoJsonTarget = {
-            type: 'Point',
-            coordinates: [Number(req.query.lng), Number(req.query.lat)]
-        };
-        var maxDistance = req.query.maxDistance ? Number(req.query.maxDistance) : 0.5;
+  if (req.query.lat && req.query.lng) { // geospatial querying
+    var geoJsonTarget = {
+      type: 'Point',
+      coordinates: [Number(req.query.lng), Number(req.query.lat)]
+    };
+    var maxDistance = req.query.maxDistance ? Number(req.query.maxDistance) : 0.5;
 
-        RestaurantModel.geoNear(geoJsonTarget, {spherical : true, maxDistance : maxDistance}, function (err, geoResults, stats) {
-            var restaurants = [];
-            for (var i = 0, length = geoResults.length; i < length; i++) {
-                var geoResult = geoResults[i];
-                var restaurant = geoResult.obj;
-                restaurant.distance = geoResult.dis;
-                restaurants.push(restaurant);
-            }
-            if (!err) {
-                return res.send(restaurants);
-            } else {
-                console.log(err);
-                return res.status(400).send(err);
-            }
-        });
-    } else { // regular query
-        return RestaurantModel.find(function (err, restaurants) {
-            if (!err) {
-                return res.send(restaurants);
-            } else {
-                console.log(err);
-                return res.status(400).send(err);
-            }
-        });
-    }
+    RestaurantModel.geoNear(geoJsonTarget, {spherical : true, maxDistance : maxDistance}, function (err, geoResults, stats) {
+      var restaurants = [];
+      for (var i = 0, length = geoResults.length; i < length; i++) {
+        var geoResult = geoResults[i];
+        var restaurant = geoResult.obj;
+        restaurant.distance = geoResult.dis;
+        restaurants.push(restaurant);
+      }
+      if (!err) {
+        return res.send(restaurants);
+      } else {
+        console.log(err);
+        return res.status(400).send(err);
+      }
+    });
+  } else { // regular query
+    return RestaurantModel.find(function (err, restaurants) {
+      if (!err) {
+        return res.send(restaurants);
+      } else {
+        console.log(err);
+        return res.status(400).send(err);
+      }
+    });
+  }
 };
 
 /* update db with remote business data */
@@ -92,13 +92,13 @@ exports.refreshAll = function (req, res){
       /* update */
       data.restaurants.forEach(function(element, index) {
         RestaurantModel.findOne({"id": element.id}, function (err, restaurant){
-          if(restaurant == null)
-          {
-            new RestaurantModel(element).save();
-          } else {
-            restaurant.set(element);
-            restaurant.save();
-          }
+          if(restaurant === null)
+            {
+              new RestaurantModel(element).save();
+            } else {
+              restaurant.set(element);
+              restaurant.save();
+            }
         });
       });
     }
@@ -114,24 +114,24 @@ exports.refreshAll = function (req, res){
  * @param res
  */
 exports.voteOnRestaurantQueue = function (req, res) {
-    SecurityService.getCurrentUser(req, res, function(user) {
-        RestaurantModel.findOne({'id': req.params.id}, function(err, restaurant) {
-            if (!err) {
-                restaurant.voteOnQueue(user, Number(req.body.timeSlotIndex));
-                restaurant.save(function(err) {
-                    if (!err) {
-                        return res.send(restaurant);
-                    } else {
-                        console.log(err);
-                        return res.status(500).send(err);
-                    }
-                });
-            } else {
-                console.log(err);
-                return res.status(400).send(err);
-            }
+  SecurityService.getCurrentUser(req, res, function(user) {
+    RestaurantModel.findOne({'id': req.params.id}, function(err, restaurant) {
+      if (!err) {
+        restaurant.voteOnQueue(user, Number(req.body.timeSlotIndex));
+        restaurant.save(function(err) {
+          if (!err) {
+            return res.send(restaurant);
+          } else {
+            console.log(err);
+            return res.status(500).send(err);
+          }
         });
+      } else {
+        console.log(err);
+        return res.status(400).send(err);
+      }
     });
+  });
 };
 
 /**
@@ -141,21 +141,21 @@ exports.voteOnRestaurantQueue = function (req, res) {
  * @param res
  */
 exports.updateRestaurantMenu = function(req, res) {
-    RestaurantModel.findOne({'id': req.params.id}, function(err, restaurant) {
+  RestaurantModel.findOne({'id': req.params.id}, function(err, restaurant) {
+    if (!err) {
+      // update restaurant queue
+      restaurant.menus = req.body.menus;
+      restaurant.save(function(err) {
         if (!err) {
-            // update restaurant queue
-            restaurant.menus = req.body.menus;
-            restaurant.save(function(err) {
-                if (!err) {
-                    return res.send(restaurant);
-                } else {
-                    console.log(err);
-                    return res.status(400).send(err);
-                }
-            });
+          return res.send(restaurant);
         } else {
-            console.log(err);
-            return res.status(400).send(err);
+          console.log(err);
+          return res.status(400).send(err);
         }
-    });
+      });
+    } else {
+      console.log(err);
+      return res.status(400).send(err);
+    }
+  });
 };
