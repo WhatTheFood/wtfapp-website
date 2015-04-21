d02fe09f62ef79f85b6b793a1edfb26363210aa2
Adrien Giboire <adrien.giboire@gmail.com>
2015-04-21 11:47:57 +0200

    style: respect js coding convention

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // allow cross domain
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
-    next();
-}
+  res.header('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+  next();
+};
 app.use(allowCrossDomain);
 
 // Use the passport package in our application
@@ -45,52 +45,49 @@ app.use('/api/restaurants', restaurants);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.send(401, 'invalid token...');
-    }
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  if (err.name === 'UnauthorizedError') {
+    res.send(401, 'invalid token...');
+  }
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // database connection
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOLAB_URI + '/wtfapp');
 
-//
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
-    var restaurants = db.collection('restaurants');
-    restaurants.ensureIndex({geolocation: '2dsphere'}, {}, function(err, result) {
-        if(err) {
-            return console.dir(err);
-        }
-    });
+  var restaurants = db.collection('restaurants');
+  restaurants.ensureIndex({geolocation: '2dsphere'}, {}, function(err, result) {
+    if(err) {
+      return console.dir(err);
+    }
+  });
 });
-
-// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
