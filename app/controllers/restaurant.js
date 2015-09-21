2192143d6deea4b2ab000a2332a0567a85886a57
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-09-21 19:15:17 +0200

    Quickfix

--- app/controllers/restaurant.js
+++ app/controllers/restaurant.js
@@ -89,7 +89,8 @@ exports.refreshAll = function (req, res){
   /* get json file and parse it */
   // ori : http://www.stockcrous.fr/static/json/crous-paris.min.json
   // fake : https://s3-eu-west-1.amazonaws.com/crousdata.whatthefood/fakecrous.min.js
-  request('https://s3-eu-west-1.amazonaws.com/crousdata.whatthefood/fakecrous.min.js', function (error, response, body) {
+  // old fake : http://thepbm.ovh.org/static/json/crous-poitiers.min.json
+  request('http://thepbm.ovh.org/static/json/crous-poitiers.min.json', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body.replace(new RegExp('\r?\n','g'), ' '));
 
